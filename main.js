1886d8814b540e8aec0493bc7345ee97e74cce27
Diwakar Kumar <diwakar@MacBook-Pro.local>
2024-01-16 17:13:01 +0530

    Unshift method polyfill for js

--- main.js
+++ main.js
@@ -1,1 +1,25 @@
-console.log('Hello');
+if (!Array.prototype.unshift) {
+    Array.prototype.myUnshift = function () {
+      const argumentsCount = arguments.length;
+      const totalElementInArray = this.length;
+      const arr = this;
+  
+      if (argumentsCount > 0) {
+         // Move existing elements to make space for new elements
+        for (let index = totalElementInArray - 1; index >= 0; index--) {
+          arr[index + argumentsCount] = this[index];
+        }
+  
+        // Insert new elements at the beginning
+        for (let index = 0; index < argumentsCount; index++) {
+          arr[index] = arguments[index];
+        }
+      }
+  
+      return arr.length;
+    };
+  }
+  
+  let arr = [1, 2, 3, 4];
+  
+  arr.myUnshift(-1, 0);
